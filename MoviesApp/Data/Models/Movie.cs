f606e59878c809f056661a523e11449c9624d938
Ervis Trupja <ervistrupja@outlook.com>
2022-05-07 22:47:18 +0200

    One way data binding

---
+++ MoviesApp/Data/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesApp.Data.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Rate { get; set; }
+        public string Description { get; set; }
+    }
+}
--- MoviesApp/Pages/Movies.cshtml.cs
+++ MoviesApp/Pages/Movies.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoviesApp.Data.Models;
 
 namespace MoviesApp.Pages
 {
     public class MoviesModel : PageModel
     {
+        public List<Movie> Movies { get; set; }
+
         public void OnGet()
         {
+            Movies = new List<Movie>()
+            {
+                new Movie() {
+                    Id = 1,
+                    Title = "First movie title",
+                    Rate = 10,
+                    Description = "First movie description goes here..."
+                },new Movie() {
+                    Id = 2,
+                    Title = "Second movie title",
+                    Rate = 7,
+                    Description = "Second movie description goes here..."
+                }, new Movie() {
+                    Id = 3,
+                    Title = "Third movie title",
+                    Rate = 5,
+                    Description = "Third movie description goes here..."
+                }, new Movie() {
+                    Id = 4,
+                    Title = "Forth movie title",
+                    Rate = 10,
+                    Description = "Forth movie description goes here..."
+                }
+            };
         }
     }
 }
