b7aa28edcfaef024655f0abc8ed24014e596f1c4
Ervis Trupja <ervistrupja@outlook.com>
2022-05-07 22:01:28 +0200

    Updated

---
+++ MoviesApp/Pages/Movie.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoviesApp.Pages
+{
+    public class MovieModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MoviesApp/Pages/Movies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoviesApp.Pages
+{
+    public class MoviesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
