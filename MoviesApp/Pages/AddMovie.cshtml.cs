b216aa968197a3d01ea8c9117168ca5cde8b7925
Ervis Trupja <ervistrupja@outlook.com>
2022-05-07 23:02:00 +0200

    Two way data binding

--- MoviesApp/Pages/AddMovie.cshtml.cs
+++ MoviesApp/Pages/AddMovie.cshtml.cs
@@ -5,8 +5,26 @@ namespace MoviesApp.Pages
 {
     public class AddMovieModel : PageModel
     {
+        [BindProperty]
+        public string Title { get; set; }
+
+        [BindProperty]
+        public int Rate { get; set; }
+
+        [BindProperty]
+        public string Description { get; set; }
+
         public void OnGet()
         {
+            Title = "Welcome";
+        }
+
+        public IActionResult OnPost()
+        {
+            string value = $"{Title} - {Rate} - {Description}";
+
+            return Page();
+            //return Redirect("Movies");
         }
     }
 }
