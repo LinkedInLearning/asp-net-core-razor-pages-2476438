d61c1f6e68e4629f91fc3e5e5f7840fa39fda2b9
Ervis Trupja <ervistrupja@outlook.com>
2022-05-08 23:34:05 +0200

    Getting single data

--- MoviesApp/Pages/Movie.cshtml.cs
+++ MoviesApp/Pages/Movie.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoviesApp.Data;
+using MoviesApp.Data.Models;
 
 namespace MoviesApp.Pages
 {
     public class MovieModel : PageModel
     {
-        public void OnGet()
+        public Movie? Movie { get; set; }
+
+
+        private ApplicationDbContext _context;
+        public MovieModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public void OnGet(int id)
+        {
+            Movie = _context.Movies.FirstOrDefault(n => n.Id == id);
         }
     }
 }
